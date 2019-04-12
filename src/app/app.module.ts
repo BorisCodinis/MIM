e7f9cc3f28753bad2fc3e11761dedb73d10c8e37
BorisCodinis <boris.naefe@hotmail.de>
2019-04-12 14:13:23 +0200

    basic navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { RouterModule, Routes } from '@angular/router';
+import { WirComponent } from './wir/wir.component';
+import { ProjekteComponent } from './projekte/projekte.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { SpendenComponent } from './spenden/spenden.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { StartseiteComponent } from './startseite/startseite.component';
+
+
+const appRoutes: Routes = [
+  { path: 'start', component: StartseiteComponent },
+  { path: 'wir', component: WirComponent},
+  {
+    path: 'projekte',
+    component: ProjekteComponent
+  },
+  { path: 'kontakt',
+    component: KontaktComponent
+  },
+  {
+    path: 'spenden',
+    component: SpendenComponent
+  },
+  {
+    path: 'impressum',
+    component: ImpressumComponent
+  },
+  { path: '',
+    redirectTo: '/start',
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: 'start' }
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WirComponent,
+    ProjekteComponent,
+    KontaktComponent,
+    SpendenComponent,
+    ImpressumComponent,
+    StartseiteComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AngularFontAwesomeModule
   ],
---
+++ src/app/wir/wir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wir',
+  templateUrl: './wir.component.html',
+  styleUrls: ['./wir.component.css']
+})
+export class WirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
